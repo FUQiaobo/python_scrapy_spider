24c5a314777aef6f3f108e1c341189c15ee392d4
一休哥哥哥哥哥 <30581354+fu9852531@users.noreply.github.com>
2017-12-26 20:03:14 -0600

    scrapy 配置文件
    2017.12.27 第一版    爬取糗事百科

---
+++ qiushibaike/qiushibaike/pipelines.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import time
+import os.path
+
+class QiushiPipeline(object):
+    def process_item(self, item, spider):
+        today = time.strftime('%Y%m%d',time.localtime())
+        Filename = today+'.txt'
+        with open(Filename,'a') as f:
+            #f.write(item['id']+'\t')
+            f.write(item['text']+'\t\n')
+            #f.write(item['good']+'\n')
+            time.sleep(2)
+        return item
